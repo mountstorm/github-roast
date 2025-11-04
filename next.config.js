21f331808e758c4d05784ecc6c0df567635903bc
mountstorm <mkhaydarov17@gmail.com>
2025-11-04 11:49:42 -0600

    Fix basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/github-roast' : '',
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    unoptimized: true,
   },
 }
 
